bdb147d6585115398b380f5953b691852ff18cf1
Eugene Pobochny <eu-ge-ne@users.noreply.github.com>
2025-06-11 15:48:21 -0400

    test: c0, c1 (#10)

--- test/ascii.test.ts
+++ test/ascii.test.ts
@@ -1,9 +1,6 @@
 import { assertEquals } from "@std/assert";
-
 import { wcswidth } from "../src/wcswidth.ts";
 
 Deno.test("ASCII", () => {
-  const width = wcswidth("Lorem ipsum");
-
-  assertEquals(width, 11);
+  assertEquals(wcswidth("Lorem ipsum"), 11);
 });
